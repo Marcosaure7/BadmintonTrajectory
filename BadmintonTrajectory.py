bc352d442a1dc2e4cb81581c46b525119ab486ff
Marcosaure7 <magou277@ulaval.ca>
2024-10-28 03:54:45 -0400

    Rennomé fichier traitant de la trajectoire de la vitesse et de l'angle ; Implémentation du polynôme de Newton complétée et fonctionnel pour 3 points donnés.

--- BadmintonTrajectory.py
+++ BadmintonTrajectory.py
@@ -1,48 +1,67 @@
-
-import numpy as np
 import matplotlib.pyplot as plt
 
+def poly_newton(x, y, h = 0.1):
+    """
+    Calcule le polynôme de Newton engendré des points passés en paramètres.
 
-class BadmintonTrajectory:
-    def __init__(self, vitesse_ini, angle):
-        self.pos_x_list = []
-        self.pos_y_list = []
-        self.affichage_des_coo(vitesse_ini, angle)
+    Paramètres :
+        x (list): Liste des abscisses des points.
+        y (list): Liste des ordonnées des points.
+        h (float) : "Pas" en x utilisé pour construire le tableau de points en sortie
 
-    def affichage_des_coo(self, vitesse_ini, angle):
-        angle_rad = np.radians(angle)
-        vitesse_x = np.cos(angle_rad) * vitesse_ini
-        vitesse_y = np.sin(angle_rad) * vitesse_ini
-        pos_x = 0
-        pos_y = 1
-        dt = 0.001
-        vitesse_x_prec = vitesse_x
+    Retour :
+        x_newton (list): Liste des abscisses des points du polynôme de Newton. 
+        y_newton (list): Liste des ordonnées des points du polynôme de Newton. 
+    """
+    n = len(x)
 
-        self.pos_x_list.append(pos_x)
-        self.pos_y_list.append(pos_y)
-
-        # Mise à jour des positions et vitesses
-        while pos_y >= 0:
-            vitesse_x += acceleration_x(vitesse_x, vitesse_y, dt)
-            vitesse_y += acceleration_y(vitesse_x_prec, vitesse_y, dt)
-            vitesse_x_prec = vitesse_x
-
-            pos_x += vitesse_x * dt
-            pos_y += vitesse_y * dt
-
-            self.pos_x_list.append(pos_x)
-            self.pos_y_list.append(pos_y)
+    # Tableau des différences divisées selon : {f[x0], f[x0, x1], ... , f[x0, ..., xn]}
+    coeffecients = [difference_divisee(x, y, 0, i) for i in range (n)]
 
 
-def acceleration_x(vitesse_x, vitesse_y, increment):
-    return -0.22 * vitesse_x * np.sqrt(vitesse_x**2 + vitesse_y**2) * increment
+    # Retourne le facteur en forme de (x-x0)(x-x1)... d'un terme d'un polynôme de Newton
+    def facteur_en_x_moins_xi(n, x_cible):
+        retour = 1
+        for i in range (n): retour *= (x_cible - x[i])
+        return retour
 
-def acceleration_y(vitesse_x, vitesse_y, increment):
-    return -(9.81 + 0.22 * vitesse_y * np.sqrt(vitesse_x**2 + vitesse_y**2)) * increment
+    nb_points = int((x[-1] - x[0])/h)
+    x_newton = [x[0] + i*h for i in range (nb_points)]    
+    y_newton = []
 
-def plot_trajectory(trajet):
+    for i in range (nb_points):
+        y_courant = 0
+        for j in range (len(coeffecients)):
+            y_courant += coeffecients[j] * facteur_en_x_moins_xi(j, x_newton[i])
+            
+        y_newton.append(y_courant)
+
+    return x_newton, y_newton
+    
+
+def difference_divisee(x, y, i, j):
+    """
+    Calcule la différence divisée f[x_i, ..., x_j] pour les points (x, y).
+    
+    Paramètres :
+        x (list): Liste des abscisses des points.
+        y (list): Liste des ordonnées des points.
+        i (int): Indice de début pour la différence divisée.
+        j (int): Indice de fin pour la différence divisée.
+    
+    Retour :
+        float: La différence divisée f[x_i, ..., x_j].
+    """
+    # Cas de base : différence divisée d'ordre 0
+    if i == j:
+        return y[i]
+    
+    # Appel récursif pour calculer les différences d'ordre > 0
+    return ((difference_divisee(x, y, i + 1, j) - difference_divisee(x, y, i, j - 1))/(x[j] - x[i]))
+
+def plot_trajectory(x, y):
     plt.figure(figsize=(12, 6))
-    plt.plot(trajet.pos_x_list, trajet.pos_y_list, color='blue')
+    plt.plot(x, y, color='blue')
 
     # Définition des étiquettes et du titre
     plt.title('Trajectoire du volant de badminton')
@@ -51,6 +70,9 @@ def plot_trajectory(trajet):
     plt.grid(True)
     plt.show()
 
-if __name__ == '__main__':
-    traj = BadmintonTrajectory(33.33, 35)
-    plot_trajectory(traj)
+    
+
+
+if __name__ == "__main__":
+    x, y = poly_newton([0, 3, 6], [1, 3, 0])
+    plot_trajectory(x, y)
